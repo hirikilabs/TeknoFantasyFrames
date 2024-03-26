b8f84553a8a11c19348b29686c3a216110c3af91
David Pello <dvpg@tuta.io>
2024-03-26 16:05:46 +0100

    Deprecated NewMax, larger window size and added spacer, cleanup.

--- frames.go
+++ frames.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"strconv"
 	"os"
-	
+	"strconv"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
 func extractFrames(video string, folder string, step float64) error {
 	err := ffmpeg.Input(video, ffmpeg.KwArgs{}).
-		Output(folder + string(os.PathSeparator) +
-		"frame_%03d.jpg", ffmpeg.KwArgs{"vf": "select=not(mod(n\\," +
+		Output(folder+string(os.PathSeparator)+
+			"frame_%03d.jpg", ffmpeg.KwArgs{"vf": "select=not(mod(n\\," +
 			strconv.FormatFloat(step, 'f', 0, 64) +
-			"))", "vsync" : "vfr"}).
+			"))", "vsync": "vfr"}).
 		OverWriteOutput().ErrorToStdOut().Run()
 
 	return err
@@ -32,16 +32,16 @@ func main() {
 	outputFolder := ""
 	inputSelected := false
 	outputSelected := false
-	
+
 	frameStep := 5.0
-	
+
 	// create GUI
 	a := app.New()
 	a.Settings().SetTheme(theme.DarkTheme())
 
 	// window and size
 	w := a.NewWindow("Frames - TeknoFantasy")
-	w.Resize(fyne.NewSize(300, 300))
+	w.Resize(fyne.NewSize(300, 400))
 
 	// image background
 	bgImg := canvas.NewImageFromFile("logo.jpg")
@@ -49,7 +49,7 @@ func main() {
 
 	// widgets
 	// input
-	fileLabel := widget.NewLabel("");
+	fileLabel := widget.NewLabel("")
 	openLabel := widget.NewLabel("Open Video")
 	openButton := widget.NewButton("...", func() {
 		dialog.ShowFileOpen(func(u fyne.URIReadCloser, err error) {
@@ -80,7 +80,7 @@ func main() {
 				outputSelected = true
 			}
 		}, w)
-		
+
 	})
 	outputContainer := container.New(layout.NewHBoxLayout(), outputLabel, outputButton)
 
@@ -99,10 +99,10 @@ func main() {
 		if inputSelected && outputSelected {
 			runButton.SetText("Running...")
 			runButton.Disable()
-			
+
 			// launch ffmpeg command
 			err := extractFrames(videoFile, outputFolder, frameStep)
-			
+
 			if err != nil {
 				dialog.ShowError(err, w)
 			}
@@ -110,15 +110,15 @@ func main() {
 			runButton.SetText("RUN")
 			runButton.Enable()
 		}
-		
+
 	}
 	runButton.OnTapped = runButtonFunc
-	
+
 	content := container.New(layout.NewVBoxLayout(), openContainer, fileLabel,
 		widget.NewSeparator(), outputContainer, folderLabel,
 		widget.NewSeparator(), sliderContainer,
-		widget.NewSeparator(), runButton)
-	
-	w.SetContent(container.NewMax(bgImg, content))
+		widget.NewSeparator(), layout.NewSpacer(), runButton)
+
+	w.SetContent(container.NewStack(bgImg, content))
 	w.ShowAndRun()
 }
